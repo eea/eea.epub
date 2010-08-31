5f69b2d5614b689d44ef325210550786b98d7eef
thuliper <thuliper@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-08-31 09:53:08 +0000

    Fix bug where context could not be reached within the page template

--- eea/epub/browser/export.py
+++ eea/epub/browser/export.py
@@ -1,22 +1,18 @@
 from StringIO import StringIO
 from zipfile import ZipFile
-from zope.app.pagetemplate.viewpagetemplatefile import ViewPageTemplateFile
+from zope.app.pagetemplate import ViewPageTemplateFile
 from Products.Five import BrowserView
 
 class ExportView(BrowserView):
 
     template = ViewPageTemplateFile('epub.pt')
 
-    def __init__(self, context, request):
-        self.context = context
-        self.request = request
-
     def __call__(self):
         response = self.request.response
         response.setHeader('Content-Type', 'application/xml+epub')
         response.setHeader('Content-Disposition', 'attachment; filename=%s.epub' % self.context.id)
 
-        templateOutput = self.template().encode('utf-8')
+        templateOutput = self.template(self).encode('utf-8')
         inMemoryOutputFile = StringIO()
 
         zipFile = ZipFile(inMemoryOutputFile, 'w')
