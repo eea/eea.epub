49227d5e521dadc19209953fde7a35b059ca067a
thuliper <thuliper@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-09-16 12:36:03 +0000

    Cleanup

--- eea/epub/tests/testImporter.py
+++ eea/epub/tests/testImporter.py
@@ -19,28 +19,29 @@ class ImporterTest(EEAMegaTestCase):
         view = self.portal.restrictedTraverse('@@epub_import_view')
         view.importFile(fileContent)
 
+        self.rootEpubFolder = getattr(self.portal, 'climate-change-impact-in-europe', None)
+
     def test_folderStructure(self):
-        rootEpubFolder = getattr(self.portal, 'climate-change-impact-in-europe', None)
-        self.failUnless(rootEpubFolder is not None)
-        self.failUnless(IImportedBook.providedBy(rootEpubFolder))
-        brains = rootEpubFolder.getFolderContents({'portal_type':'Article'})
+        self.failUnless(self.rootEpubFolder is not None)
+        self.failUnless(IImportedBook.providedBy(self.rootEpubFolder))
+
+        # There should be one imported chapter
+        brains = self.rootEpubFolder.getFolderContents({'portal_type':'Article'})
         self.failUnless(len(brains) == 1) # EPUB contained one chapter
+
         chapter = brains[0]
         self.failUnless(IImportedChapter.providedBy(chapter.getObject()))
         self.failUnless(chapter['id'] == 'chapter1.xhtml')
         self.failUnless(chapter['Title'] == 'Text')
 
     def test_coverImage(self):
-        rootEpubFolder = getattr(self.portal, 'climate-change-impact-in-europe', None)
-        self.failUnless(rootEpubFolder.epub_cover.portal_type == 'Image')
+        self.failUnless(self.rootEpubFolder.epub_cover.portal_type == 'Image')
 
     def test_creator(self):
-        rootEpubFolder = getattr(self.portal, 'climate-change-impact-in-europe', None)
-        self.failUnless(rootEpubFolder.Creator() == 'S\xc3\xb8ren Roug')
+        self.failUnless(self.rootEpubFolder.Creator() == 'S\xc3\xb8ren Roug')
 
     def test_imagesImportedCorrectly(self):
-        rootEpubFolder = getattr(self.portal, 'climate-change-impact-in-europe', None)
-        brains = rootEpubFolder['Pictures'].getFolderContents({'portal_type': 'Image'})
+        brains = self.rootEpubFolder['Pictures'].getFolderContents({'portal_type': 'Image'})
         self.failUnless(IImportedImage.providedBy(brains[0].getObject()))
         self.failUnless(len(brains) == 9)
 
