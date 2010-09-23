ced274dd8ee84ca71406ed60bb3ef88910e96081
thuliper <thuliper@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-09-23 13:27:07 +0000

    Refactored logic into a 'utils' browser view [eea.epub]

--- eea/epub/browser/epub_toc_logic.py
+++ eea/epub/browser/epub_toc_logic.py
@@ -1,21 +1,11 @@
 from zope.app.annotation.interfaces import IAnnotations
-from Acquisition import aq_base
 from Products.Five import BrowserView
-from Products.CMFCore.utils import getToolByName
-from Products.CMFPlone import utils
-from eea.epub.interfaces import IImportedBook
 
 class EpubTocLogic(BrowserView):
 
     def getNavPoints(self):
-        # 1. find ebook
-        obj = self.context
-        portal_url = getToolByName(obj, 'portal_url')	    
-        portal = portal_url.getPortalObject()
-        while not IImportedBook.providedBy(obj) and aq_base(obj) is not aq_base(portal):
-            obj = utils.parent(obj)
-
-        # 2. extract toc
-        annotations = IAnnotations(obj)
+        utils = self.context.restrictedTraverse('@@epub_utils')
+        ebook = utils.getEbook()
+        annotations = IAnnotations(ebook)
         mapping = annotations.get('eea.epub')
         return mapping['toc']
--- eea/epub/browser/interfaces.py
+++ eea/epub/browser/interfaces.py
@@ -15,3 +15,14 @@ class IEpubTocLogic(Interface):
 
     def getNavPoints():
         pass
+
+class IEpubUtils(Interface):
+
+    def getEbook():
+        pass
+
+    def isImportedEbook():
+        pass
+
+    def isImportedChapter():
+        pass
