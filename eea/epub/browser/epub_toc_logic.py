c225f5fb8f85bad729f229b22efbc51e4219b70d
thuliper <thuliper@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-09-23 09:37:07 +0000

    Make portlet_epub also work for epub chapters, refs #7633

--- eea/epub/browser/epub_toc_logic.py
+++ eea/epub/browser/epub_toc_logic.py
@@ -1,9 +1,21 @@
-from Products.Five import BrowserView
 from zope.app.annotation.interfaces import IAnnotations
+from Acquisition import aq_base
+from Products.Five import BrowserView
+from Products.CMFCore.utils import getToolByName
+from Products.CMFPlone import utils
+from eea.epub.interfaces import IImportedBook
 
 class EpubTocLogic(BrowserView):
 
     def getNavPoints(self):
-        annotations = IAnnotations(self.context.getCanonical())
+        # 1. find ebook
+        obj = self.context
+        portal_url = getToolByName(obj, 'portal_url')	    
+        portal = portal_url.getPortalObject()
+        while not IImportedBook.providedBy(obj) and aq_base(obj) is not aq_base(portal):
+            obj = utils.parent(obj)
+
+        # 2. extract toc
+        annotations = IAnnotations(obj)
         mapping = annotations.get('eea.epub')
         return mapping['toc']
