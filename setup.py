dd0efaaaf80b617bd88047c72bcacc47df7e87ae
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-04-27 11:21:21 +0000

    Refs #8196; Added buildout for plone 4 for eea.epub

--- setup.py
+++ setup.py
@@ -9,19 +9,22 @@ version = open(join(*path)).read().strip()
 
 setup(name='eea.epub',
       version=version,
-      description="",
+      description="Publish Plone content in epub form",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
+        "Framework :: Plone",
         "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='epub plone import',
-      author='David Ichim (Edw), Per Thulin, Antonio De Marinis (EEA), European Environment Agency (EEA)',
+      author='Per Thulin, David Ichim (eaudeweb), Tiberiu Ichim (eaudeweb), '
+             'Antonio De Marinis (EEA), European Environment Agency (EEA)',
       author_email='webadmin@eea.europa.eu',
       url='https://svn.eionet.europa.eu/projects/Zope',
       license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
+      packages=find_packages(exclude=['ez_setup', 'tests']),
       namespace_packages=['eea'],
       include_package_data=True,
       zip_safe=False,
